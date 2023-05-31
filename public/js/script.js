0c80f26f3533ae5cecb3b597697ef9a02100664e
Navdeep-kaur1 <59167625+Navdeep-code@users.noreply.github.com>
2023-05-31 13:23:34 -0400

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,7 @@ var navlink = document.querySelectorAll('header nav a');
 for (var i = 0; i < navlink.length; i++) {
     navlink[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace("active", "");
+        current[0].className = current[0].className.replace(" active", "");
         this.className += "active";
     });
 }
